b73cdcde869ca9022b2490288f59c9971d026e33
Vasiliy Karavaev <vasiliykaravaev@gmail.com>
2022-08-23 16:12:31 +0530

    добаваили

---
+++ 34/Program.cs
@@ -0,0 +1,44 @@
+﻿
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу,
+//  которая покажет количество чётных чисел в массиве.
+// _____________________________________________________________________________________________________________________
+
+ int size = Convert.ToInt32(Console.ReadLine()); 
+ int [] array = new int[size];      
+ for (int i = 0; i < array.Length; i++) 
+ {
+   array[i] = new Random().Next(0, 1000);
+   System.Console.Write($"{array[i]}, ");
+ }
+ 
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+ Console.WriteLine("Вот наш массив: ");
+ PrintArray(numbers);
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+if (numbers[i] % 2 == 0)
+count++;
+
+Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
