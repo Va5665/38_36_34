adb86220be8d2c7a8399a8d7f575cd0323529a2e
Vasiliy Karavaev <vasiliykaravaev@gmail.com>
2022-08-25 22:46:21 +0530

    добаваили

--- 36/obj/Debug/net6.0/36.AssemblyInfo.cs
+++ 36/obj/Debug/net6.0/36.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("34.1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("36")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("34.1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("34.1")]
+[assembly: System.Reflection.AssemblyProductAttribute("36")]
+[assembly: System.Reflection.AssemblyTitleAttribute("36")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
